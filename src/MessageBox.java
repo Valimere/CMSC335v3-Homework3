9640ab9446bcffc89c60f6f0f0bf5e723e56b329
Valimere <Valimere@gmail.com>
2015-04-19 15:38:44 -0400

    Initial commit

---
+++ src/MessageBox.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+/**
+ * file name
+ * date      4/19/15
+ * author    Stephen Drollinger
+ * purpose
+ */
+public class MessageBox {
+    private JLabel select;
+    private JRadioButton red;
+    private JRadioButton yellow;
+    private JRadioButton white;
+    private JRadioButton grey;
+    private JRadioButton green;
+    private JTextField welcomeToJavaTextField;
+    private JButton button1;
+    private JButton button2;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
